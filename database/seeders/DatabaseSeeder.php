02a7c9b79bf7fdbdde050efb92a84551d6c7c27c
bert093 <sas992203@gmail.com>
2025-12-30 13:47:04 +0800

    chore(seeder): add initial users and rooms data
    - Password are stored using Hash::make so the password is hashed for
    privacy
    - is_admin set to false for Regular User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Hash;
+use App\Models\Room;
+
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
@@ -15,11 +18,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //* UNTUK ADMIN
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true
+        ]);
+
+        //* UNTUK USER BIASA
+        User::create([
+            'name' => 'User Test',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => false
+        ]);
+
+        Room::create([
+            'name' => 'Ruang Meeting A', 'location' => 'Lantai 1', 'capacity' => 10
+        ]);
+
+        Room::create([
+            'name' => 'Ruang Meeting B', 'location' => 'Lantai 2', 'capacity' => 20
+        ]);
+
+        Room::create([
+            'name' => 'Ruang Meeting C', 'location' => 'Lantai 3', 'capacity' => 30
+        ]);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
